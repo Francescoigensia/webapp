9df2fb253435d9cda092fee4fde111b75a27f5cb
Francescoigensia <f.campanella1@edu.igensia.com>
2026-07-02 10:37:56 +0200

    Refactor tests in app.test.js
    Updated tests to include new cases and changed import statement.

--- app.test.js
+++ app.test.js
@@ -1,11 +1,4 @@
-const calcul = require('./app');
-
-// Ton test existant (qui couvre le 'else')
-test('adds 1 + 2 to equal 3', () => {
-  expect(calcul(1, 2)).toBe(3);
-});
-
-// NOUVEAU TEST : pour couvrir le 'if (a > 10)'
-test('adds numbers when a is greater than 10', () => {
-  expect(calcul(11, 2)).toBe(13);
+const { calcul } = require('./app');
+test('2 + 3 = 5', () => {
+  expect(calcul(2, 3)).toBe(5);
 });
