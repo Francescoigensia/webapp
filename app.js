5161ba5bf4ac131681a6ea33618636aadd0fa5a9
Francescoigensia <f.campanella1@edu.igensia.com>
2026-07-02 12:14:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function calcul(a, b) {
-    var inutilisee = 0;
-    if (a > 10) { return a + b; }
-    else { return a + b; }
+    if (a > 10) {
+        return a + b;
+    }
+    return a + b;
 }
 module.exports = calcul;
