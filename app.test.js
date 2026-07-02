35f4ff848f013311bc82214a5b32dc2c0347bd42
Francescoigensia <f.campanella1@edu.igensia.com>
2026-07-02 10:07:43 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,5 +1,11 @@
 const calcul = require('./app');
 
+// Ton test existant (qui couvre le 'else')
 test('adds 1 + 2 to equal 3', () => {
-    expect(calcul(1, 2)).toBe(3);
+  expect(calcul(1, 2)).toBe(3);
+});
+
+// NOUVEAU TEST : pour couvrir le 'if (a > 10)'
+test('adds numbers when a is greater than 10', () => {
+  expect(calcul(11, 2)).toBe(13);
 });
