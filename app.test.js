0a431081cf9b757fda70d01ca7edbd015db13b05
Francescoigensia <f.campanella1@edu.igensia.com>
2026-07-02 12:02:23 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -4,7 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(calcul(1, 2)).toBe(3);
 });
 
-// Ajout du test pour couvrir le "if (a > 10)"
 test('adds 15 + 5 to equal 20', () => {
     expect(calcul(15, 5)).toBe(20);
 });
